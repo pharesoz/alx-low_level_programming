3eb65bd2656123ed2ada0772296a6e49bd7207c6
pharesoz <118558357+pharesoz@users.noreply.github.com>
2022-12-09 00:21:36 +0300

    commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+/**
+ * main - starting point
+ *
+ * Description : prints size of various tpes
+ *
+ * Return:  0 -> indicates ind of program
+ *
+ */
+int main(void)
+{
+	char ch;
+	int in;
+	long int loin;
+	long long int loloin;
+	float fl;
+
+	printf("Size of a char: %d byte(s)\n", sizeof(ch));
+	printf("Size of an int: %d byte(s)\n", sizeof(in));
+	printf("Size of a long int: %d byte(s)\n", sizeof(loin));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(loloin));
+	printf("Size of a float: %d byte(s)\n", sizeof(fl));
+
+	return (0);
+}

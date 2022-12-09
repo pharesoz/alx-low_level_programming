00d49bd18149886e4233333a5d9ca1bed0fd3dfc
pharesoz <118558357+pharesoz@users.noreply.github.com>
2022-12-09 22:23:52 +0300

    erro

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+/**
+ * main - prints the alphabets
+ *
+ * Description: prints out a-z
+ *
+ * Return: 0-> indicate end
+ *
+ */
+int main(void)
+{
+	char alpha[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+			'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+			'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alpha[i]);
+	}
+	putchar('\n');
+	return (0);
+}
